4b88c4a10cb3f80b7e32982a51c0884879bb64aa
nick-beznos <beznosn@gmail.com>
2020-02-11 10:50:53 +0200

    Initial Commit

---
+++ Vanga Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vanga Weather
+//
+//  Created by Nick Beznos on 2/11/20.
+//  Copyright © 2020 Nick Beznos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
